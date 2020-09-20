a03b305aec30fb67f92970f5a5d94b4a7e7d7441
Jasprit Singh Gill <jaspritsgill@gmail.com>
2020-09-20 12:45:34 -0700

    Simplify the switching between the aggressive and passive drive scenario using a single variable

--- sim_scenarios/contextual_IMM_main.m
+++ sim_scenarios/contextual_IMM_main.m
@@ -13,14 +13,13 @@ ctx_imm.driverTypes = [0.5 0.5];
 sigma_r = 0.05;
 enable_measurement_noise = true;
 
-%aggressive driver run
-%initial_front_car_distance = 100;
-%leftLaneFollowingVehicleInitPosition = -106;
-
-%Passive driver run
-initial_front_car_distance = 80;
-leftLaneFollowingVehicleInitPosition = -25;
-
+if aggressive_driver_use_case == true
+    initial_front_car_distance = 100;
+    leftLaneFollowingVehicleInitPosition = -106;
+else
+    initial_front_car_distance = 80;
+    leftLaneFollowingVehicleInitPosition = -25;
+end
 
 for i = 1:length(simtime)
     t = simtime(i);
